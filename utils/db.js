4209e20cee28059b06446804b83b47f4ad1a90e6
timmoh-king <timothykariuki43@gmail.com>
2023-12-08 13:51:42 +0300

    Task 1: MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,28 @@
+import MongoClient from 'mongodb/lib/mongo_client';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const databaseURL = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new MongoClient(databaseURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
