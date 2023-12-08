49ee3781543d4983236b432a58230203ec49c5d3
timmoh-king <timothykariuki43@gmail.com>
2023-12-08 11:23:24 +0300

    task 0: 0. Redis utils

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import redisClient from './utils/redis';
 (async () => {
   console.log(redisClient.isAlive());
   console.log(await redisClient.get('myKey'));
-  console.log('__________________________----------------');
   await redisClient.set('myKey', 12, 5);
   console.log(await redisClient.get('myKey'));
 
